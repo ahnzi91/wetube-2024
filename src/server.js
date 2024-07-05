d82e86b157c5a71dbb1eaefb46a3254b81fafd8d
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-07-05 23:28:37 +0900

    #4.1 Making Our Routers

--- src/server.js
+++ src/server.js
@@ -6,20 +6,26 @@ const PORT = 4000;
 // // Creates An Express Application
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-    console.log("I will respond.");
-    return res.send("hello");
-};
-
-const login = (req, res) => {
-    return res.send("login");
-};
-
 app.use(logger);
 
-app.get("/", home);
-app.get("/login", login);
+// Create An Global Router
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+// Create An User Router
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+// Create An Video Router
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 

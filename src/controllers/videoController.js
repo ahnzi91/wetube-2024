421abecd7cb077da0dc5c96c0609761a20dba88e
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-07-17 13:47:10 +0900

    #6.27 Search part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -80,11 +80,16 @@ export const deleteVideo = async (req, res) => {
     return res.redirect("/");
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
     const { keyword } = req.query;
-    console.log("Should search for : ", keyword);
+    let videos = [];
     if (keyword) {
         // search
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(keyword, "i")
+            },
+        });
     }
-    return res.render("search", { pageTitle: "Search" });
+    return res.render("search", { pageTitle: "Search", videos });
 };

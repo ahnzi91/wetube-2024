aaf1f9d19d63c32b6e11dee387e8ddb72350e7a4
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-07-19 11:35:45 +0900

    #7.1 Create Account part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,21 @@
-export const join = (req, res) => res.send("join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => {
+    res.render("join", { pageTitle: "Create Account" });
+};
+
+export const postJoin = async (req, res) => {
+    const { name, username, email, password, location } = req.body;
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location,
+    });
+    return res.redirect("/login");
+};
+
 export const edit = (req, res) => res.send("edit");
 export const remove = (req, res) => res.send("Remove");
 export const login = (req, res) => res.send("Login");
